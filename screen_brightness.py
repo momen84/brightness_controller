c1de4174a57e8e34e523f2c86a63cbe02c294dd1
R0Wi <ro.windey@gmail.com>
2021-01-06 10:26:35 +0100

    Immediate value update on slider change

--- screen_brightness.py
+++ screen_brightness.py
@@ -6,7 +6,6 @@
 
 import subprocess
 from tkinter import *
-	
 
 def get_display_name():
 
@@ -32,24 +31,24 @@ def get_current_brightness(event):
 def center(root):
 	h=root.winfo_screenheight()
 	w=root.winfo_screenwidth()
-	root.geometry("+%d+%d" % (w/2, h/2))
+	root.geometry("+%d+%d" % (w/2 - 300, h/2 - 25))
 
 root = Tk()
 center(root)
 root.title("Screen Brightness")
+root.minsize(width=600, height=50)
 scalevar = IntVar()
-frame=Frame(root,width=50, height=50)
-scale = Scale(frame, from_=0, to=100, variable=scalevar, orient="horizontal")
-button=Button(frame,text='Apply',width=50)
+frame=Frame(root,width=600, height=50)
+scale = Scale(frame, from_=0, to=100, variable=scalevar, orient="horizontal", command=get_scale_value)
 
 root.update_idletasks()
 scalevar.set(get_current_brightness(None)*100)
 
 root.update()
 scale.pack(side="top", fill="x", expand=True)
-button.pack(side="bottom", fill="x", expand=True)
 
-button.bind("<Button-1>",get_scale_value)
-frame.pack()
+frame.pack(expand=True, fill='both')
+frame.pack_propagate(0)
 
 root.mainloop()
+
